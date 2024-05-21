6dc1385e334052121c092b35f336c8e79a16580b
augustin <augustin.denuce@gmail.com>
2024-05-21 12:31:03 +0200

    print

--- print.c
+++ print.c
@@ -10,8 +10,40 @@
 // |xx bbjvr|
 // +--------+
 
+//♞ ■♞ ■ ♟
 
 void print_board()
 {
-    printf(BLUE_ANSI"■"RESET_ANSI);
+    //printf(B_BLUE_ANSI"■\n♞    ■■"RESET_ANSI);
+    
+    
+    char empty[] = " ";
+    char *tmp[5]; // not null terminated
+    for (int i = 0; i < 5; i++)
+    {
+        tmp[i] = empty;
+    }
+
+#ifdef USE_ANSI
+    char r[] = RED_ANSI"♞"RESET_ANSI;
+    char b[] = BLUE_ANSI"♞"RESET_ANSI;
+    char y[] = YELLOW_ANSI"♞"RESET_ANSI;
+    char g[] = GREEN_ANSI"♞"RESET_ANSI;
+#endif
+#ifndef USE_ANSI
+    char r[] = RED_ANSI"r"RESET_ANSI;
+    char b[] = BLUE_ANSI"b"RESET_ANSI;
+    char y[] = YELLOW_ANSI"j"RESET_ANSI;
+    char g[] = GREEN_ANSI"v"RESET_ANSI;
+#endif
+
+
+            
+    printf("\n");
+    printf("|%02d %s%s%s%s%s|", 0, tmp[0], tmp[1], tmp[2], tmp[3], tmp[4]);
+
+
+
+
+
 }
--- print.h
+++ print.h
@@ -10,11 +10,19 @@
 #include <stdlib.h>
 
 
+#define B_BLUE_ANSI "\e[0;34m"
+#define B_YELLOW_ANSI "\e[0;34m"
+#define B_RED_ANSI "\e[0;34m"
+#define B_GREEN_ANSI "\e[0;34m"
+
 #define BLUE_ANSI "\e[34m"
-#define YELLOW_ANSI \e[34m
+#define YELLOW_ANSI "\e[34m"
+#define RED_ANSI "\e[34m"
+#define GREEN_ANSI "\e[34m"
 
 #define RESET_ANSI "\e[0m"
 
+#define USE_ANSI
 
 void print_board();
 
