0b2720b5d342f3bf166bd7a3bf6f767c1194896d
Dathuss <tjouv64@google.com>
2024-05-23 08:27:29 +0200

    Ajout fonction

--- board.h
+++ board.h
@@ -54,6 +54,7 @@ typedef struct {
 color_t getHorse(board_t, int pos);
 color_t getHorseInHouse(board_t, color c, int pos);
 int isRefuge(board_t, int pos);
-color barrageAtPos(board_t, int pos);
+color_t barrageAtPos(board_t, int pos);
+bool isMovePossible(board_t, int pos, color_t curHorseColor);
 
 #endif
